de103a907dd2291c953655f1e724d233e3052dd9
Arjun <63303108+ArjunKumarNair@users.noreply.github.com>
2025-06-24 12:15:02 +0800

    Adds python-add application with k8s deployments

--- src/app.py
+++ src/app.py
@@ -1,11 +1,14 @@
 from flask import Flask, jsonify
+import datetime
+import socket
 
 app = Flask(__name__)
 
 @app.route('/api/v1/details')
 def details():
     return jsonify({
-        'message': 'Hello World',
+        'time': datetime.datetime.now().strftime("%I:%M%p on %B %d, %Y"),
+        'hostname': socket.gethostname()
     })
 
 @app.route('/api/v1/healthz')
